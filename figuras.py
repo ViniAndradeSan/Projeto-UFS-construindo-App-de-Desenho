d687fff684c742df7ff5e3f7c9b051955315ee7e
Vinicius <viniciusandrade0521@gmail.com>
2026-07-01 10:12:16 -0300

    Implementa Retangulo, Oval e Circulo na hierarquia OO

--- figuras.py
+++ figuras.py
@@ -14,3 +14,41 @@ class Figura(ABC):
 
     def esta_incompleta(self):
         return (self.x1, self.y1) == (self.x2, self.y2)
+
+
+class FiguraPreenchida(Figura):
+    pass
+
+
+class Retangulo(FiguraPreenchida):
+    def desenhar(self, canvas, tracejado=False):
+        canvas.create_rectangle(
+            self.x1, self.y1, self.x2, self.y2,
+            **self._opcoes_desenho(tracejado)
+        )
+
+
+class Oval(FiguraPreenchida):
+    def desenhar(self, canvas, tracejado=False):
+        canvas.create_oval(
+            self.x1, self.y1, self.x2, self.y2,
+            **self._opcoes_desenho(tracejado)
+        )
+
+
+class Circulo(FiguraPreenchida):
+    def desenhar(self, canvas, tracejado=False):
+        x1, x2 = min(self.x1, self.x2), max(self.x1, self.x2)
+        y1, y2 = min(self.y1, self.y2), max(self.y1, self.y2)
+        largura = x2 - x1
+        altura = y2 - y1
+        lado = min(largura, altura)
+        centrox = (x1 + x2) // 2
+        centroy = (y1 + y2) // 2
+        raio = lado // 2
+
+        canvas.create_oval(
+            centrox - raio, centroy - raio,
+            centrox + raio, centroy + raio,
+            **self._opcoes_desenho(tracejado)
+        )
